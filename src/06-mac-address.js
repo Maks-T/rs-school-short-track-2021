3a0adaf2d786bf1c821d1a3caf9b0472d622c1ad
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2021-04-11 16:46:25 +0300

    add 6

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,17 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const arrHEX = String(n).split('-');
+  if (arrHEX.length === 6) {
+    for (let i = 0; i < arrHEX.length; i++) {
+      const numMAC = String(arrHEX[i].match(/[0-9a-fA-F][0-9a-fA-F]/));
+
+      if (numMAC.length !== 2) return false;
+    }
+    return true;
+  }
+  return false;
 }
 
 module.exports = isMAC48Address;
