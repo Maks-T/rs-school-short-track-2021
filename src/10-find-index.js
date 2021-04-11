82deefac30cf565243f6676c68a914faadd1f1e1
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2021-04-11 20:58:46 +0300

    add 10,11

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,25 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
-}
+function findIndex(array, value) {
+  let first = 0;
+  let end = array.length - 1;
+  let center = Math.floor((first + end) / 2);
 
+  if (array[first] === value) return first;
+  if (array[end] === value) return end;
+
+  while (array[center] !== value) {
+    if (array[center] > value) {
+      end = center;
+      center = Math.floor((first + end) / 2);
+    }
+
+    if (array[center] < value) {
+      first = center;
+      center = Math.floor((first + end) / 2);
+    }
+  }
+  return center;
+}
 module.exports = findIndex;
--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,27 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+
+function encodeLine(str) {
+  const arrStr = str.split('');
+  let resStr = '';
+  let curChar = arrStr[0];
+  let curCount = 1;
+
+  for (let i = 1; i < arrStr.length; i++) {
+    if (arrStr[i] === curChar) curCount++;
+    else {
+      if (curCount > 1) resStr += curCount + curChar;
+      if (curCount === 1) resStr += curChar;
+      curChar = arrStr[i];
+      curCount = 1;
+    }
+    if (i === arrStr.length - 1) {
+      if (curCount > 1) resStr += curCount + curChar;
+      if (curCount === 1) resStr += curChar;
+    }
+  }
+  return resStr;
 }
 
 module.exports = encodeLine;
